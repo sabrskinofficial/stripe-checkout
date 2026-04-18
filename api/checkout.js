b592769a0eaa1a098f93496b3e7b266b152fd694
sabrskinofficial <sabrskin.co@gmail.com>
2026-04-18 20:32:13 +1000

    Update checkout.js

--- api/checkout.js
+++ api/checkout.js
@@ -4,11 +4,13 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   // ✅ CORS
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  if (req.method === "OPTIONS") {
+if (req.method === "OPTIONS") {
+  return res.status(200).end();
+}
     return res.status(200).end();
   }
 
